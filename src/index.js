fcae989ba9ddf94dce465b349ae5a1b46341ea21
SP <74065235+SPchalil@users.noreply.github.com>
2020-12-15 01:59:12 -0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,12 +11,13 @@ function fill(cell){
             document.getElementById(cell.id).innerHTML = "o"; 
         }
         count++;
+       
         
     }
     else{
         alert ("Match Draw");
         reset();
-        count = 1;
+        
     }
 }
 
@@ -24,52 +25,6 @@ function reset(){
     for (var i=1; i<10 ; i++){
         document.getElementById('game-cell'+i).innerHTML = ""; 
     }
-    
-}
-/*--
-function checkWin()
-{
-    if (document.getElementById('game-cell1').innerHTML!= "" && document.getElementById('game-cell2').innerHTML!= "" && document.getElementById('game-cell3').innerHTML!= "" && document.getElementById('game-cell1').innerHTML==document.getElementById('game-cell2').innerHTML && document.getElementById('game-cell2').innerHTML==document.getElementById('game-cell3').innerHTML) ||  
-        document.getElementById('game-cell4').innerHTML!= "" && document.getElementById('game-cell5').innerHTML!= "" && document.getElementById('game-cell6').innerHTML!= "" && document.getElementById('game-cell4').innerHTML==document.getElementById('game-cell5').innerHTML && document.getElementById('game-cell5').innerHTML==document.getElementById('game-cell6').innerHTML) ||
-        document.getElementById('game-cell7').innerHTML!= "" && document.getElementById('game-cell8').innerHTML!= "" && document.getElementById('game-cell9').innerHTML!= "" && document.getElementById('game-cell7').innerHTML==document.getElementById('game-cell8').innerHTML && document.getElementById('game-cell8').innerHTML==document.getElementById('game-cell8').innerHTML) ||
-        document.getElementById('game-cell1').innerHTML!= "" && document.getElementById('game-cell4').innerHTML!= "" && document.getElementById('game-cell7').innerHTML!= "" && document.getElementById('game-cell1').innerHTML==document.getElementById('game-cell4').innerHTML && document.getElementById('game-cell4').innerHTML==document.getElementById('game-cell7').innerHTML) ||
-        document.getElementById('game-cell2').innerHTML!= "" && document.getElementById('game-cell5').innerHTML!= "" && document.getElementById('game-cell8').innerHTML!= "" && document.getElementById('game-cell2').innerHTML==document.getElementById('game-cell5').innerHTML && document.getElementById('game-cell5').innerHTML==document.getElementById('game-cell8').innerHTML) ||
-        document.getElementById('game-cell3').innerHTML!= "" && document.getElementById('game-cell6').innerHTML!= "" && document.getElementById('game-cell9').innerHTML!= "" && document.getElementById('game-cell3').innerHTML==document.getElementById('game-cell6').innerHTML && document.getElementById('game-cell6').innerHTML==document.getElementById('game-cell9').innerHTML) ||
-        document.getElementById('game-cell1').innerHTML!= "" && document.getElementById('game-cell5').innerHTML!= "" && document.getElementById('game-cell9').innerHTML!= "" && document.getElementById('game-cell1').innerHTML==document.getElementById('game-cell5').innerHTML && document.getElementById('game-cell5').innerHTML==document.getElementById('game-cell9').innerHTML) ||
-        document.getElementById('game-cell3').innerHTML!= "" && document.getElementById('game-cell5').innerHTML!= "" && document.getElementById('game-cell7').innerHTML!= "" && document.getElementById('game-cell3').innerHTML==document.getElementById('game-cell5').innerHTML && document.getElementById('game-cell5').innerHTML==document.getElementById('game-cell7').innerHTML) 
-        {
-            return true;
-        
-        }
-    
-}   
-*/
-
-/*
-if (checkWin()){
-    alert ("Winner");
-    reset(); 
     count = 1;
-}   
-function checkWin()
-    {
-        if (checkCondition('game-cell1','game-cell2','game-cell3')|| checkCondition('game-cell4','game-cell5','game-cell6')||checkCondition('game-cell7','game-cell8','game-cell9')||checkCondition('game-cell1','game-cell4','game-cell7') || checkCondition('game-cell2','game-cell5','game-cell8') || checkCondition('game-cell3','game-cell6','game-cell9') || checkCondition('game-cell1','game-cell5','game-cell9') || checkCondition('game-cell3','game-cell5','game-cell7'))
-        {
-            return true;
-        
-        }
-    
-    } 
-    
-/*
-function checkCondition(x1,x2,x3)
-    {
-        if (document.getElementById(x1).innerHTML!= "" && document.getElementById(x2).innerHTML!= "" && document.getElementById('x3').innerHTML!= "" && document.getElementById(x1).innerHTML==document.getElementById(x2).innerHTML && document.getElementById(x2).innerHTML==document.getElementById(x3).innerHTML) 
-        {
-            return true;
-        
-        }
-    
-    }   
+}
 
-    */
