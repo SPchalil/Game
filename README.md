c682b8f22302299f708a1b578333835c691c141d
SP <74065235+SPchalil@users.noreply.github.com>
2021-03-01 11:36:06 -0330

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Game
+# Game -TicTacToe
+
+A simple tic-tac-toe game developed, as practice of learning HTML, CSS and Javascript.
+
+A project that has been undertaken as an initial step towards learning and understanding of using pure Javascript methods.
  
+### How to run
+
+You can clone the repo and use LiveServer in VSCode to run index.html and check out an offline version on your local machine.
--- src/index.js
+++ src/index.js
@@ -1,195 +1,116 @@
-
-
-
-
-
-/*-----------------------------------------------------------------------------------------*/
-/*
-count =1;
-function fill(number){  
-           
-    let board = document.getElementById('game-cell'+number); 
-    console.log (board) ;
-
-    //board.innerHTML = "x";
-    if (count <= 9){
-        if (count % 2 !=0)  {
-           
-            board.innerHTML = "×";  
-            
-        }
-        else{
-            board.innerHTML = "o"; 
-            
-        }
-        count++;
-       
-        
-    }
-    else{
-        alert ("Match Draw");
-        reset();
-        
-    }
-}
-
-function reset(){
-    for (var i=0; i<9 ; i++){
-        document.getElementById('game-cell'+i).innerHTML = ""; 
-    }
-    count = 1;
-}
-
-*/
+/*-----------------------------------Tic-Tac-Toe Game logic------------------------------------------------------*/
 
 let currentPlayer = 'X';
-//let count = 1;
+
 let display = document.getElementById('status2');
-function fill(number){         
-    let board = document.getElementById('game-cell'+number);
-    
+function fill(number) {
+    let board = document.getElementById('game-cell' + number);
+
     if (board.innerHTML == "") {
         board.innerHTML = currentPlayer;
         checkPlayer();
-        
-       /* 
-        if (winner()){
-            //window.alert("Winner is" + currentPlayer);
-            display.innerHTML = "Winner is " + currentPlayer; 
-            reset();
-        }
-        else {
-            display.innerHTML = "Next turn: Player " + currentPlayer; 
-        } */
-        //count ++; 
-    } 
-   
-    else{
 
-        if (tie()){
+    }
+
+    else {
+
+        if (tie()) {
             display.innerHTML = "Tie/Game ended";
             reset();
         }
-        //window.alert("Tie-Game ended") 
-       
-        
+
     }
-    
+
 }
 
-function checkPlayer(){
-   
-    if (currentPlayer == 'X' ){ 
-        
-        //currentPlayer = 'O';
-        if (winner()){
-            //window.alert("Winner is" + currentPlayer);
-            
-            display.innerHTML = "Winner is " + currentPlayer ; 
+function checkPlayer() {
+
+    if (currentPlayer == 'X') {
+
+        if (winner()) {
+            display.innerHTML = "Winner is " + currentPlayer;
             reset();
-
-            
         }
-
-        
         else {
             currentPlayer = 'O';
-            
-            display.innerHTML = "Next turn: Player " + currentPlayer; 
-            
+            display.innerHTML = "Next turn: Player " + currentPlayer;
         }
-    
-       
-    }   
+    }
     else {
-        //currentPlayer = 'X'; 
-        
-        if (winner()){
-            
-            //window.alert("Winner is" + currentPlayer);
-            display.innerHTML = "Winner is Player " + currentPlayer; 
+        if (winner()) {
+            display.innerHTML = "Winner is Player " + currentPlayer;
             reset();
         }
-
-        
         else {
             currentPlayer = 'X';
-            
-            display.innerHTML = "Next turn: Player " + currentPlayer; 
-            
+            display.innerHTML = "Next turn: Player " + currentPlayer;
         }
-        
-    }
-
-       
-}
-
-function reset(){
-    for (var i=0; i<9 ; i++){
-        document.getElementById('game-cell'+i).innerHTML = ""; 
     }
 }
 
-function restart(){
-    for (var i=0; i<9 ; i++){
-        document.getElementById('game-cell'+i).innerHTML = ""; 
+function reset() {
+    for (var i = 0; i < 9; i++) {
+        document.getElementById('game-cell' + i).innerHTML = "";
     }
-    //count = 1;
+}
+
+function restart() {
+    for (var i = 0; i < 9; i++) {
+        document.getElementById('game-cell' + i).innerHTML = "";
+    }
+
     currentPlayer = 'X';
     display.innerHTML = "Let's Play";
 }
 
-function getDiv(num){
-    return document.getElementById('game-cell'+num).innerHTML;
+function getDiv(num) {
+    return document.getElementById('game-cell' + num).innerHTML;
 }
 
-function checkDivs(a,b,c,m){
-    if (getDiv(a)==m && getDiv(b)==m && getDiv(c)==m){
+function checkDivs(a, b, c, m) {
+    if (getDiv(a) == m && getDiv(b) == m && getDiv(c) == m) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
-function winner (){
-    
-    if (checkDivs(0,1,2,currentPlayer)|| checkDivs(3,4,5,currentPlayer) 
-    || checkDivs(6,7,8,currentPlayer) || checkDivs(0,3,6,currentPlayer) 
-    || checkDivs(1,4,7,currentPlayer) || checkDivs(2,5,8,currentPlayer)
-    || checkDivs(0,4,8,currentPlayer) || checkDivs(2,4,6,currentPlayer)){
-        //window.alert ("Winner is" + currentPlayer);
-      return true;
-        //reset();
+function winner() {
+
+    if (checkDivs(0, 1, 2, currentPlayer) || checkDivs(3, 4, 5, currentPlayer)
+        || checkDivs(6, 7, 8, currentPlayer) || checkDivs(0, 3, 6, currentPlayer)
+        || checkDivs(1, 4, 7, currentPlayer) || checkDivs(2, 5, 8, currentPlayer)
+        || checkDivs(0, 4, 8, currentPlayer) || checkDivs(2, 4, 6, currentPlayer)) {
+        return true;
     }
- 
-    else{
+    else {
         return false;
-        
+
     }
 
-   
+
 }
 
-function tie(){
-    for (var i=0; i<9 ; i++){
-        if (getDiv(i) != ""){
+function tie() {
+    for (var i = 0; i < 9; i++) {
+        if (getDiv(i) != "") {
             return true;
-        }; 
+        };
     }
 }
 
-    
 
 
 
 
 
-           
- 
-    
 
 
 
-  
+
+
+
+
+
 
 
